cd95c36320cf286a1e89b70d314e5e90c75a342b
wismo-s <stepha2128.af@gmail.com>
2023-10-07 12:58:59 -0500

    login logic fisinsh

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -7,9 +9,26 @@ def home(request):
     return render(request, 'index.html')
 
 def singup(request):
-    return render(request, 'singup.html', {
-        'form': UserCreationForm
-    })
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create(username = request.POST['username'], password = request.POST['password1'])
+                user.save()
+                return redirect('singupdone')
+            except:
+                return render(request, 'singup.html', {
+                    'form': UserCreationForm,
+                    'error': 'el usuario ya existe'
+                })
+        else:
+            return render(request, 'singup.html', {
+                    'form': UserCreationForm,
+                    'error': 'no conicide las contrasenas'
+                })
+    else:
+        return render(request, 'singup.html', {
+            'form': UserCreationForm
+        })
 
 def singupdone(request):
     return render(request, 'singupdone.html')
